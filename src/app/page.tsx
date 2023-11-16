8b38331639332b49582fc53e63439029e29a3b2a
Gabriel Roberti <gabriel_rti@hotmail.com>
2023-11-16 16:46:20 -0300

    configurando as rotas, os códigos, a tratativa dos textos, a criação de uma array para salvar somente os textos sem as tags, uma leitura dos textos por aspas, a utilização nativa do js para ler os textos, a configuração de avançar, voltar e pausar com espaço e as setas durante a leitura do texto e a exibição da página no frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,61 +1,121 @@
+// src/pages/legis.tsx
 'use client'
 import { useGetLegisAll } from '@/api/legis';
 import { legisItem } from '@/types/legis';
-import { executePythonScript } from '@/utils/axios'; // Certifique-se de importar corretamente o caminho para o seu arquivo axios
-import { useEffect, useState } from 'react';
+import { executePythonCodigo, executePythonScript } from '@/utils/axios';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
-
-const tableHead = ["Titulo", "Descrição", "Ano"]
-
-
-function Legis() {
-
-  const [url] = useState('http://www.planalto.gov.br/ccivil_03/Leis/2002/L10406.htm'); // Estado para armazenar a URL
-
-const handleExecutePythonScript = async () => {
-  try {
-    await executePythonScript(url); // Envie a URL como parâmetro
-    // Adicione qualquer lógica adicional após a execução do script Python, se necessário
-  } catch (error) {
-    console.error('Erro ao executar o script Python:', error);
-  }
-};
-
+const Legis: React.FC = () => {
   const [legisData, setLegisData] = useState<legisItem[]>([]);
-  const {legis} = useGetLegisAll();
+  const { legis } = useGetLegisAll();
+  const textContainerRef = useRef<HTMLDivElement>(null);
+  const textsRef = useRef<string[]>([]);
+  const currentTextIndexRef = useRef(0);
+  const speedRef = useRef(1);
+  const speakingRef = useRef(false);
 
   useEffect(() => {
-
-    if(legis.length){
-      setLegisData(legis);
+    if (legis && legis.length > 0) {
+      const specificLegis = legis.find((item) => item.titulo === 'Teste de request');
+      setLegisData(specificLegis ? [specificLegis] : []);
     }
-  }
-  ,
-  [legis]
-  )
-  return (
+  }, [legis]);
 
-    <div>
+  useEffect(() => {
+    if (legisData.length > 0) {
+      const descriptionHtml = legisData[0].descricao || '';
+      const tempDiv = document.createElement('div');
+      tempDiv.innerHTML = descriptionHtml;
+
+      const textArray: string[] = [];
+      tempDiv.querySelectorAll('p').forEach((paragraph) => {
+        const text = paragraph.textContent?.trim();
+        if (text) {
+          textArray.push(text);
+        }
+      });
+
+      textsRef.current = textArray;
+    }
+  }, [legisData]);
+
+  const readText = useCallback((text: string) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'pt-BR';
+    utterance.rate = speedRef.current;
+    speakingRef.current = true;
+
+    utterance.onend = () => {
+      speakingRef.current = false;
+      if (currentTextIndexRef.current < textsRef.current.length - 1) {
+        currentTextIndexRef.current++;
+        readText(textsRef.current[currentTextIndexRef.current]);
+      }
+    };
+
+    window.speechSynthesis.speak(utterance);
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'ArrowRight' && currentTextIndexRef.current < textsRef.current.length - 1) {
+        window.speechSynthesis.cancel();
+        currentTextIndexRef.current++;
+        readText(textsRef.current[currentTextIndexRef.current]);
+      } else if (event.key === 'ArrowLeft' && currentTextIndexRef.current > 0) {
+        window.speechSynthesis.cancel();
+        currentTextIndexRef.current--;
+        readText(textsRef.current[currentTextIndexRef.current]);
+      } else if (event.key === ' ') {
+        event.preventDefault();
+        if (speakingRef.current) {
+          window.speechSynthesis.pause();
+          speakingRef.current = false;
+        } else {
+          window.speechSynthesis.resume();
+          speakingRef.current = true;
+          readText(textsRef.current[currentTextIndexRef.current]);
+        }
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [readText]);
+
+  const handleExecutePythonScript = async () => {
+    try {
+      await executePythonScript();
+      setLegisData(legis);
+    } catch (error) {
+      console.error('Erro ao executar o script Python:', error);
+    }
+  };
+
+  const handleExecutePythonCodigo = async () => {
+    try {
+      await executePythonCodigo();
+      setLegisData(legis);
+    } catch (error) {
+      console.error('Erro ao executar o script Python:', error);
+    }
+  };
+
+  if (!legisData.length) {
+    return <p>Nenhuma legislação encontrada.</p>;
+  }
+
+  return (
+    <div ref={textContainerRef}>
       <h1>Legislação:</h1>
       <button onClick={handleExecutePythonScript}>Executar Script Python</button>
-      <table>
-        <thead>
-          <tr>{tableHead.map((head) => (
-            <th key={head}>{head}</th>
-          ))}
-          </tr>
-        </thead>
-      </table>
-      <ul>
-      </ul>
-      <ul>
-        {legisData.map((legis) => (
-          <li key={legis._id}>{legis.titulo}</li>
-          
-        ))}
-      </ul>
+      <button onClick={handleExecutePythonCodigo}>Executar Script Codigo</button>
+      <div dangerouslySetInnerHTML={{ __html: legisData[0].descricao }} />
     </div>
   );
-}
+};
 
 export default Legis;
